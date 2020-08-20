008c70043ed027e2226fc0dc4d15b0db70b61db1
Yasser Kadhim <yasser.kadhim@gmail.com>
2020-08-20 22:44:30 +0200

    Updates

---
+++ precache-manifest.17230ebb6adb6547922ee2c5d1860b4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f0d7a26282bee3b700f3620662c6b46",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "c6b63cde7d233703de28",
+    "url": "/tic-tac-toe/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "a2436778992cd372315b",
+    "url": "/tic-tac-toe/static/js/2.b9926c31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.b9926c31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6b63cde7d233703de28",
+    "url": "/tic-tac-toe/static/js/main.625687af.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
